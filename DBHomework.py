4b9563ccb5932cc8140d145c589b0f88f0a3ae3f
Mansur Sambiev <52548855+MansurSambiev@users.noreply.github.com>
2024-10-17 18:19:28 +0300

    Update IGNORE and functions

--- DBHomework.py
+++ DBHomework.py
@@ -7,7 +7,7 @@ cursor = conn.cursor()
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS items (
 id INTEGER PRIMARY KEY,
-name VARCHAR(48),
+name VARCHAR(48) UNIQUE,
 category VARCHAR(32),
 price INT,
 amount INT)
@@ -15,47 +15,45 @@ amount INT)
 
 conn.commit()
 items = [
-  ('ARDOR GAMING', 'игровая', 4099, 5),
-  ('A4TECH', 'игровая', 2399, 8),
-  ('Redragon Apas', 'игровая', 6499, 12),
-  ('Logitech G413', 'игровая', 6999, 6),
-  ('Ritmix', 'офисная', 650, 20),
-  ('ExeGate', 'офисная', 1500, 10),
-  ('Aceline', 'офисная', 850, 15)
+    ('ARDOR GAMING', 'игровая', 4099, 5),
+    ('A4TECH', 'игровая', 2399, 8),
+    ('Redragon Apas', 'игровая', 6499, 12),
+    ('Logitech G413', 'игровая', 6999, 6),
+    ('Ritmix', 'офисная', 650, 20),
+    ('ExeGate', 'офисная', 1500, 10),
+    ('Aceline', 'офисная', 850, 15)
 ]
-  
-  
+
 # Не писал close чтобы не комментировать каждый раз код перед выполнением следующей команды
 
-cursor.executemany("INSERT INTO items (name, category, price, amount) VALUES(?, ?, ?, ?)", items)
+cursor.executemany("INSERT OR IGNORE INTO items (name, category, price, amount) VALUES(?, ?, ?, ?)", items)
 conn.commit()
 
-#Вывести все игровые клавиатуры дороже 5000р
+def selectFromTable(condition1, condition2):
+    cursor.execute("SELECT * FROM items WHERE category = ? AND price > ?", (condition1, condition2))
+    answer = cursor.fetchall()
+    for i in answer:
+        print(i)
+    conn.commit()
 
-cursor.execute("SELECT * FROM items WHERE category = 'игровая' AND price > 5000")
-answer = cursor.fetchall()
+#Не узнал как можно передать знак сравнения в функцию, пришлось создать еще одну функцию с измененным знаком с > на <
+def selectCheaper(condition1, condition2):
+    cursor.execute("SELECT * FROM items WHERE category = ? AND price < ?", (condition1, condition2))
+    answer = cursor.fetchall()
+    for i in answer:
+        print(i)
+    conn.commit()
+
+
 print('Вывести все игровые клавиатуры дороже 5000р')
-for i in answer:
-    print(i)
-conn.commit()
+selectFromTable('игровая', 5000)
 
-#Вывести все игровые клавиатуры дороже 2000р и с количеством больше 3
-cursor.execute("SELECT * FROM items WHERE category = 'игровая' AND price > 2000 AND amount > 3")
-answer = cursor.fetchall()
 print('Вывести все игровые клавиатуры дороже 2000р и с количеством больше 3')
-for i in answer:
-    print(i)
-conn.commit()
+selectFromTable('игровая', 2000)
 
-#Вывести все офисные клавиатуры дешевле 1000р
-cursor.execute("SELECT * FROM items WHERE category = 'офисная' AND price < 1000")
-answer = cursor.fetchall()
 print('Вывести все офисные клавиатуры дешевле 1000р')
-for i in answer:
-    print(i)
-conn.commit()
+selectCheaper('офисная', 1000)
 
-#Вывести офисную модель Exegate
 cursor.execute("SELECT * FROM items WHERE category = 'офисная' AND name = 'ExeGate' ")
 # или cursor.execute("SELECT * FROM items WHERE id = 6")
 answer = cursor.fetchall()
