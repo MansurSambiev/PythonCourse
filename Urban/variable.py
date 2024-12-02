1c5bf99a11261ea99ed1860564d290fab60a57a8
Mansur Sambiev <52548855+MansurSambiev@users.noreply.github.com>
2024-12-02 11:29:07 +0300

    Create variable.py

---
+++ Urban/variable.py
@@ -0,0 +1,6 @@
+count_of_homework = 12
+spent_time = 1.5
+name_of_course = 'Python'
+time_to_homework = count_of_homework / spent_time
+
+print('Курс:', name_of_course, 'всего задач',count_of_homework, 'затрачено часов:', spent_time, 'среднее время выполнения', time_to_homework,'часов')
